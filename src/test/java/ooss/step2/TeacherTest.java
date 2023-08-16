24df5685f4fc6a6bba10c2da9f8d37c4c33a449c
yamboni1 <ncyambot.work@gmail.com>
2023-08-16 19:08:55 +0800

    should_return_message_with_name_and_age_when_introduce_given_teacher

--- src/test/java/ooss/step2/TeacherTest.java
+++ src/test/java/ooss/step2/TeacherTest.java
@@ -7,18 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class TeacherTest {
-//
-//    @Test
-//    public void should_return_message_with_name_and_age_when_introduce_given_teacher() {
-//        Teacher jerry = new Teacher(1, "Jerry", 21);
-//        String introduce = jerry.introduce();
-//
-//        Teacher robert = new Teacher(1, "Robert", 20);
-//        String robertIntroduce = robert.introduce();
-//
-//        assertEquals("My name is Jerry. I am 21 years old. I am a teacher.", introduce);
-//        assertEquals("My name is Robert. I am 20 years old. I am a teacher.", robertIntroduce);
-//    }
+
+    @Test
+    public void should_return_message_with_name_and_age_when_introduce_given_teacher() {
+        Teacher jerry = new Teacher(1, "Jerry", 21);
+        String introduce = jerry.introduce();
+
+        Teacher robert = new Teacher(1, "Robert", 20);
+        String robertIntroduce = robert.introduce();
+
+        assertEquals("My name is Jerry. I am 21 years old. I am a teacher.", introduce);
+        assertEquals("My name is Robert. I am 20 years old. I am a teacher.", robertIntroduce);
+    }
 //
 //    @Test
 //    public void should_return_false_when_equals_given_two_teachers_with_different_id() {
