69dfcee3b1a7c9030640d425d77e61eb45ceafa5
yamboni1 <ncyambot.work@gmail.com>
2023-08-16 19:23:41 +0800

    feat: should_return_false_when_equals_given_two_klass_with_different_id

--- src/main/java/ooss/Klass.java
+++ src/main/java/ooss/Klass.java
@@ -1,5 +1,25 @@
 package ooss;
 
 public class Klass {
+    private final int number;
 
+    public Klass(int number){
+
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Klass klass = (Klass) o;
+
+        return number == klass.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return number;
+    }
 }
--- src/test/java/ooss/step3/KlassTest.java
+++ src/test/java/ooss/step3/KlassTest.java
@@ -8,15 +8,15 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class KlassTest {
 
-//    @Test
-//    public void should_return_false_when_equals_given_two_klass_with_different_id() {
-//        Klass klass1 = new Klass(1);
-//        Klass klass2 = new Klass(1);
-//
-//        Klass klass3 = new Klass(1);
-//        Klass klass4 = new Klass(2);
-//
-//        assertEquals(klass1, klass2);
-//        assertNotEquals(klass3, klass4);
-//    }
+    @Test
+    public void should_return_false_when_equals_given_two_klass_with_different_id() {
+        Klass klass1 = new Klass(1);
+        Klass klass2 = new Klass(1);
+
+        Klass klass3 = new Klass(1);
+        Klass klass4 = new Klass(2);
+
+        assertEquals(klass1, klass2);
+        assertNotEquals(klass3, klass4);
+    }
 }
