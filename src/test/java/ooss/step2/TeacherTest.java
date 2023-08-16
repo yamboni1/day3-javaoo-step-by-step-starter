b11485d7844be003af4cb5ed9630027f9ad9c55e
yamboni1 <ncyambot.work@gmail.com>
2023-08-16 19:09:36 +0800

    should_return_false_when_equals_given_two_teachers_with_different_id

--- src/test/java/ooss/step2/TeacherTest.java
+++ src/test/java/ooss/step2/TeacherTest.java
@@ -19,16 +19,16 @@ public class TeacherTest {
         assertEquals("My name is Jerry. I am 21 years old. I am a teacher.", introduce);
         assertEquals("My name is Robert. I am 20 years old. I am a teacher.", robertIntroduce);
     }
-//
-//    @Test
-//    public void should_return_false_when_equals_given_two_teachers_with_different_id() {
-//        Teacher teacher1 = new Teacher(1, "Jerry", 21);
-//        Teacher teacher2 = new Teacher(1, "Jerry", 22);
-//
-//        Teacher teacher3 = new Teacher(1, "Jerry", 21);
-//        Teacher teacher4 = new Teacher(2, "Jerry", 21);
-//
-//        assertEquals(teacher1, teacher2);
-//        assertNotEquals(teacher3, teacher4);
-//    }
+
+    @Test
+    public void should_return_false_when_equals_given_two_teachers_with_different_id() {
+        Teacher teacher1 = new Teacher(1, "Jerry", 21);
+        Teacher teacher2 = new Teacher(1, "Jerry", 22);
+
+        Teacher teacher3 = new Teacher(1, "Jerry", 21);
+        Teacher teacher4 = new Teacher(2, "Jerry", 21);
+
+        assertEquals(teacher1, teacher2);
+        assertNotEquals(teacher3, teacher4);
+    }
 }
