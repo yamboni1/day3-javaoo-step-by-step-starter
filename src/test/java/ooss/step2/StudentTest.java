5d4f272f2945172c7c88279106c940ec48172798
yamboni1 <ncyambot.work@gmail.com>
2023-08-16 19:08:18 +0800

    feat: should_return_is_equals_by_id_when_equals_given_two_students_with_id

--- src/test/java/ooss/step2/StudentTest.java
+++ src/test/java/ooss/step2/StudentTest.java
@@ -19,16 +19,16 @@ public class StudentTest {
         assertEquals("My name is Tom. I am 18 years old. I am a student.", tomIntroduce);
         assertEquals("My name is Jim. I am 19 years old. I am a student.", jimIntroduce);
     }
-//
-//    @Test
-//    public void should_return_is_equals_by_id_when_equals_given_two_students_with_id() {
-//        Student student1 = new Student(1, "Tom", 18);
-//        Student student2 = new Student(1, "Tommy", 19);
-//
-//        Student student3 = new Student(1, "Tom", 18);
-//        Student student4 = new Student(2, "Tom", 18);
-//
-//        assertEquals(student1, student2);
-//        assertNotEquals(student3, student4);
-//    }
+
+    @Test
+    public void should_return_is_equals_by_id_when_equals_given_two_students_with_id() {
+        Student student1 = new Student(1, "Tom", 18);
+        Student student2 = new Student(1, "Tommy", 19);
+
+        Student student3 = new Student(1, "Tom", 18);
+        Student student4 = new Student(2, "Tom", 18);
+
+        assertEquals(student1, student2);
+        assertNotEquals(student3, student4);
+    }
 }
