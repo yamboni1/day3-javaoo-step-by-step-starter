9a08e4dc7966fbfb8994cc45c5a1306492526543
yamboni1 <ncyambot.work@gmail.com>
2023-08-16 19:07:43 +0800

    feat: should_return_message_with_name_and_age_when_introduce_given_student

--- src/test/java/ooss/step2/StudentTest.java
+++ src/test/java/ooss/step2/StudentTest.java
@@ -7,18 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class StudentTest {
-//
-//    @Test
-//    public void should_return_message_with_name_and_age_when_introduce_given_student() {
-//        Student tom = new Student(1, "Tom", 18);
-//        String tomIntroduce = tom.introduce();
-//
-//        Student jim = new Student(2, "Jim", 19);
-//        String jimIntroduce = jim.introduce();
-//
-//        assertEquals("My name is Tom. I am 18 years old. I am a student.", tomIntroduce);
-//        assertEquals("My name is Jim. I am 19 years old. I am a student.", jimIntroduce);
-//    }
+
+    @Test
+    public void should_return_message_with_name_and_age_when_introduce_given_student() {
+        Student tom = new Student(1, "Tom", 18);
+        String tomIntroduce = tom.introduce();
+
+        Student jim = new Student(2, "Jim", 19);
+        String jimIntroduce = jim.introduce();
+
+        assertEquals("My name is Tom. I am 18 years old. I am a student.", tomIntroduce);
+        assertEquals("My name is Jim. I am 19 years old. I am a student.", jimIntroduce);
+    }
 //
 //    @Test
 //    public void should_return_is_equals_by_id_when_equals_given_two_students_with_id() {
