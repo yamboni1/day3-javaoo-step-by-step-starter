fd592a9e7486c1feee6a8c15c6345c54a9c938a4
yamboni1 <ncyambot.work@gmail.com>
2023-08-16 20:31:21 +0800

    feat: Should_only_in_the_recently_joined_class_when_isIn_given_student_join_two_classes_in_turn

--- src/test/java/ooss/step3/StudentTest.java
+++ src/test/java/ooss/step3/StudentTest.java
@@ -22,22 +22,22 @@ public class StudentTest {
         assertFalse(tomIn);
         assertTrue(jimIn);
     }
-//
-//    @Test
-//    public void Should_only_in_the_recently_joined_class_when_isIn_given_student_join_two_classes_in_turn() {
-//        Student tom = new Student(1, "Tom", 18);
-//        Klass klass1 = new Klass(1);
-//        Klass klass2 = new Klass(2);
-//        tom.join(klass1);
-//        tom.join(klass2);
-//
-//        boolean isInKlass1 = tom.isIn(klass1);
-//        boolean isInKlass2 = tom.isIn(klass2);
-//
-//
-//        assertFalse(isInKlass1);
-//        assertTrue(isInKlass2);
-//    }
+
+    @Test
+    public void Should_only_in_the_recently_joined_class_when_isIn_given_student_join_two_classes_in_turn() {
+        Student tom = new Student(1, "Tom", 18);
+        Klass klass1 = new Klass(1);
+        Klass klass2 = new Klass(2);
+        tom.join(klass1);
+        tom.join(klass2);
+
+        boolean isInKlass1 = tom.isIn(klass1);
+        boolean isInKlass2 = tom.isIn(klass2);
+
+
+        assertFalse(isInKlass1);
+        assertTrue(isInKlass2);
+    }
 //
 //    @Test
 //    public void should_return_message_with_name_age_and_class_when_introduce_given_student_is_in_a_class() {
